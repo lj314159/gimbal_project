0b7916a07c1bdf273b4a009c2389bd74752058f4
Layne Jones <jones.layne555@gmail.com>
2026-03-22 22:47:07 -0400

    clang formatted

--- GimbalController.h
+++ GimbalController.h
@@ -5,43 +5,41 @@
 
 class GimbalController {
 public:
-    GimbalController(uint8_t panId,
-                     uint8_t tiltId,
-                     int dirPin,
-                     HardwareSerial& dxlSerial);
+  GimbalController(uint8_t panId, uint8_t tiltId, int dirPin,
+                   HardwareSerial &dxlSerial);
 
-    void init();
-    void update();
+  void init();
+  void update();
 
 private:
-    uint8_t m_panId;
-    uint8_t m_tiltId;
+  uint8_t m_panId;
+  uint8_t m_tiltId;
 
-    int m_dirPin;
-    HardwareSerial& m_serial;
-    Dynamixel2Arduino* m_dxl;
+  int m_dirPin;
+  HardwareSerial &m_serial;
+  Dynamixel2Arduino *m_dxl;
 
-    int32_t m_zeroPan;
-    int32_t m_zeroTilt;
+  int32_t m_zeroPan;
+  int32_t m_zeroTilt;
 
-    bool m_panCal;
-    bool m_tiltCal;
+  bool m_panCal;
+  bool m_tiltCal;
 
-    bool m_panTorque;
-    bool m_tiltTorque;
+  bool m_panTorque;
+  bool m_tiltTorque;
 
-    String m_buffer;
+  String m_buffer;
 
-    void handleSerial();
-    void processCommand(String line);
+  void handleSerial();
+  void processCommand(String line);
 
-    void enableTorque(uint8_t id);
-    void disableTorque(uint8_t id);
-    void calibrate(uint8_t id);
-    void move(uint8_t id, float deg);
-    void moveBoth(float pan, float tilt);
-    void statusAll();
-    void runTest();
+  void enableTorque(uint8_t id);
+  void disableTorque(uint8_t id);
+  void calibrate(uint8_t id);
+  void move(uint8_t id, float deg);
+  void moveBoth(float pan, float tilt);
+  void statusAll();
+  void runTest();
 
-    int32_t degToTicks(float deg);
+  int32_t degToTicks(float deg);
 };
